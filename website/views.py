76c53f9075e355a99a91d760433fe86a012c94db
zach-inf <zachary.engle@infinitive.com>
2023-04-29 00:11:56 -0400

    add other templates, not complete

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Don't need to share the name with the file, but helps with consistency
 views = Blueprint('views', __name__)
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 # Define page with a decorator
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
